cbb9e61a0b6553e46a46494b4213e9342e17b584
Aayush Shrestha <95739774+AayuStha@users.noreply.github.com>
2022-06-09 09:15:18 +0545

    Update task.py

--- task.py
+++ task.py
@@ -1,17 +1,32 @@
-f = open("data.txt", "w+")
-while(True):
-
-    uName = input("Enter customer name: ")
-
-    if uName == "quit":
-        exit()
-    uAge = input("Enter customer age: ")
-    uGender = input("Enter customer gender: ")
-
-    f.write("Customer name: " f"{uName} \n")
-    f.write("Customer age: " f"{uAge} \n")
-    f.write("Customer gender: " f"{uGender} \n")
-    print("\n The data is saved. \n")
-
-    
-
+print("Enter Your Choice 1(Add)/2(Sub)/3(Divide)/4(Multiply)")
+num = int(input()
+if num == 1:
+    print("Enter Number 1 : ")
+    add1  = int(input())
+    print("Enter Number 2 : ")
+    add2 = int(input())
+    sum = add1 + add2
+    print("The Sum Is ", sum)
+elif num == 2:
+    print("Enter Number 1 : ")
+    sub1  = int(input())
+    print("Enter Number 2 : ")
+    sub2 = int(input())
+    difference = sub1 - sub2
+    print("The Difference Is ", difference)
+elif num == 3:
+    print("Enter Number 1 : ")
+    div1  = float(input())
+    print("Enter Number 2 : ")
+    div2 = float(input())
+    division = div1 / div2
+    print("The Division Is ", division)
+elif num == 4:
+    print("Enter Number 1 : ")
+    mul1 = int(input())
+    print("Enter Number 2 : ")
+    mul2 = int(input())
+    multiply = mul1 * mul2
+    print("The Difference Is ", multiply)
+else:
+    print("enter a valid Number")
